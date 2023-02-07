335a96bce0dcd56ae157399523d2aab6b0beb6c6
Tayamaki <103941533+Tayamaki@users.noreply.github.com>
2023-02-07 11:47:43 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Burger menu
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
